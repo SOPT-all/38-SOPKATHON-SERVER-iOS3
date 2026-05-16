51bb382b5a7347f2605f540273a8e500fa126e64
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-05-17 00:55:21 +0900

    feat: User, Mistake, Reflection JPA 엔티티 추가

---
+++ src/main/java/org/sopt/domain/user/entity/User.java
@@ -0,0 +1,27 @@
+package org.sopt.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.global.entity.BaseTimeEntity;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
